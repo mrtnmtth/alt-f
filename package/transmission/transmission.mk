ea818743b5f8120c0b3ec91964304dc0111794fe
jcard <jcard@cd80dc97-fa67-4d1f-9ead-3176a861659d>
2020-06-13 18:34:56 +0000

    transmission: bump to 3.00
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3909 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/transmission/transmission.mk
+++ package/transmission/transmission.mk
@@ -4,17 +4,14 @@
 #
 ###########################################################
 
-TRANSMISSION_VERSION = 2.93
+TRANSMISSION_VERSION = 3.00
 TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.xz
-TRANSMISSION_SITE = https://github.com/transmission/transmission-releases/raw/master
+TRANSMISSION_SITE = https://github.com/transmission/transmission/releases/download/3.00
 
 TRANSMISSION_AUTORECONF = NO
 TRANSMISSION_LIBTOOL_PATCH = NO
 
 TRANSMISSION_DEPENDENCIES = uclibc libcurl openssl libevent2 pkg-config
-TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2 \
---enable-utp --enable-cli --enable-lightweight
-TRANSMISSION_CONF_ENV = ZLIB_LIBS="-L$(STAGING_DIR)/usr/lib -lz" ZLIB_CFLAGS="-I$(STAGING_DIR)/usr/include"
-# FIXME: zlib: install zlib.pc
+TRANSMISSION_CONF_OPT = --disable-nls --enable-utp --enable-cli --enable-lightweight
 
 $(eval $(call AUTOTARGETS,package,transmission))
